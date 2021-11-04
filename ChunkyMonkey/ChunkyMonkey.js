f22e5fd249a78ae8a74f63fee10cb1b9e1da2eac
Daniel Vigil <daniel.julian.vigil@gmail.com>
2021-11-04 04:34:35 -0400

    Update ChunkyMonkey.js
    Refactor

--- ChunkyMonkey/ChunkyMonkey.js
+++ ChunkyMonkey/ChunkyMonkey.js
@@ -1,10 +1,10 @@
 function chunk(arr, size) {
   // Break it up.
-  var newArray = [];
-  for (var i = 0; i < arr.length; i += size){
+  let newArray = [];
+  for (let i = 0; i < arr.length; i += size){
     newArray.push(arr.slice(i, i+size));
   }
-   return newArray;
+  return newArray;
 }
 
 chunk(["a", "b", "c", "d"], 2);
