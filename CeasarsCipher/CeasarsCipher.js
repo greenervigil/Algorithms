12ce4a8f7e30e3db196e2e847b0db2db9259bdf8
Daniel Vigil <daniel.julian.vigil@gmail.com>
2021-11-04 04:31:15 -0400

    Update CeasarsCipher.js
    Refactor

--- CeasarsCipher/CeasarsCipher.js
+++ CeasarsCipher/CeasarsCipher.js
@@ -1,18 +1,15 @@
 function rot13(str) { // LBH QVQ VG!
-  var code = "";
-  for(var i=0; i<str.length; i++){
-        if(str.charCodeAt(i) < 65 || str.charCodeAt(i) > 91) {
-            code += String.fromCharCode(str.charCodeAt(i));
-            continue;
-        }
-        if(str.charCodeAt(i) < 78){
-            code += String.fromCharCode(str.charCodeAt(i) + 13);
-        }
-        else{
-            code += String.fromCharCode(str.charCodeAt(i) - 13);
-
-        }
-    }
+  let code = "";
+  for (var i=0; i<str.length; i++){
+     if (str.charCodeAt(i) < 65 || str.charCodeAt(i) > 91) {
+        code += String.fromCharCode(str.charCodeAt(i));
+        continue;
+     } else if(str.charCodeAt(i) < 78){
+        code += String.fromCharCode(str.charCodeAt(i) + 13);
+     } else{
+        code += String.fromCharCode(str.charCodeAt(i) - 13);
+     }
+  }
   return code;
 }
 
